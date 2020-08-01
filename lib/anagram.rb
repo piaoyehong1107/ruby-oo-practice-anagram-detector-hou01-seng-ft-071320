a7d5e231bfd16861aa7361fc7fa5b71fba9084d2
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-01 21:35:14 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,8 +1,10 @@
 class Anagram
-  attr_accessor :word, 
+  attr_accessor :word
+  
   def initialize(word)
     @word = word
   end
+  
   def match(anagrams)
     anagrams.each do |m|
       if word.split(" ").sort == m.split(" ").sort
