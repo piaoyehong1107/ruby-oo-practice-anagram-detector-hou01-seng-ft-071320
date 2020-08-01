15b88129c67c1c047be0c527feb7844f67d84e5a
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-01 21:46:29 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(anagrams)
+    list=[]
+    anagrams.each do |m|
+      if (word.split("").sort == m.split("").sort)
+        list.push(m)
+      end
+    end
+    list
+  end
+end
